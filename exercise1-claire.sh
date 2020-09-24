69077560adbcafee9a9686c7819bed0c9d1546bb
clairelinorush <claire.li@emory.edu>
2020-09-24 13:05:58 +0000

    Added exercise1-claire

---
+++ exercise1-claire.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Author: Claire Li
+# The script takes as input one argument, a directory, and outputs the number of different permission types in this directory.
+ls -l | cut -d ' ' -f 1 | tail --lines=+1 | uniq | wc -l
